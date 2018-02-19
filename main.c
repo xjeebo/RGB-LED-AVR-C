ca0a9228841a35610dec3e39d6b4541050a403f3
Jeeno <35685060+xjeebo@users.noreply.github.com>
2018-02-18 23:36:52 -0800

    Update main.c

--- main.c
+++ main.c
@@ -13,29 +13,29 @@
 
 int main(void) {
 
-DDRD |= (1 << 6) | ( 1 << 5);					    				// PD.6 (OC0A) and PD.6 (OC0B) is an output
-DDRB |= (1 << 3);													// PB.3 (OC2A) is an output
+DDRD |= (1 << 6) | ( 1 << 5);					      // PD.6 (OC0A) and PD.6 (OC0B) is an output
+DDRB |= (1 << 3);						     // PB.3 (OC2A) is an output
 																	// PWM with 25% duty cycle
 																	// non-inverting mode
-TCCR2A |= (1 << COM2A1);											// fast PWM mode
-TCCR2A |= (1 << WGM21) | (1 <<WGM20);								// set prescaler to 8 and enable Timer2						
+TCCR2A |= (1 << COM2A1);					  // fast PWM mode
+TCCR2A |= (1 << WGM21) | (1 <<WGM20);			       	 // set prescaler to 8 and enable Timer2						
 TCCR2B |= (1 << CS21);												
 										
-TCCR0A |= (1 << COM0A1) | ( 1 << COM0B1 );							// non-inverting mode 	// fast PWM mode											
-TCCR0A |= (1 << WGM01) | (1 <<WGM00);								// set prescaler to 8 and enable Timer0
+TCCR0A |= (1 << COM0A1) | ( 1 << COM0B1 );		      // non-inverting mode, fast PWM mode											
+TCCR0A |= (1 << WGM01) | (1 <<WGM00);			     // set prescaler to 8 and enable Timer0
 TCCR0B |= (1 << CS01);
  
  	while(1){														
-//==============================									// Continuously loop each LED output every .5 seconds
-OCR0A = 214;  // Red												// Configuration for each color code; RGB color codes
+//==============================		  	 // Continuously loop each LED output every .5 seconds
+OCR0A = 214;  // Red				        // Configuration for each color code; RGB color codes
 OCR2A = 112; // Green
 OCR0B = 219; // Blue
 PINB = ( 1 << 3 );
 PIND = (1 << 5) | ( 1 << 6);
 //==============================
-_delay_ms(500);														// 1 second = 1000 miliseconds; therefore, for 
-OCR0A = 102;  // Red												// half a second we delay by 500 milliseconds each 
-OCR2A = 102; // Green												// color change.
+_delay_ms(500);					  // 1 second = 1000 miliseconds; therefore, for 
+OCR0A = 102;  // Red				 // half a second we delay by 500 milliseconds each 
+OCR2A = 102; // Green				// color change.
 OCR0B = 255; // Blue
 PINB = ( 1 << 3 );
 PIND = (1 << 5) | ( 1 << 6);
@@ -63,5 +63,5 @@ PIND = (1 << 5) | ( 1 << 6);
 //==============================
 _delay_ms(500);
 
-}
+ }
 }
